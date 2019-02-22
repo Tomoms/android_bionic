spsforks/android-bionic-libc

Switch to FreeBSD's actual sincos.
Before:
  BM_math_sin_fast                48 ns         48 ns   14693053
  BM_math_sincos                  61 ns         61 ns   11470219

After:
  BM_math_sin_fast                48 ns         48 ns   14725120
  BM_math_sincos                  43 ns         43 ns   16329843

Bug: N/A
Test: ran tests, benchmarks
Change-Id: I8693c64135233c0641af5302c38748f47ac76737

benchmarks/math_benchmark.cpp
@@ -220,3 +220,13 @@ static void BM_math_fabs(benchmark::State& state) {
   SetLabel(state);
 }
 BIONIC_BENCHMARK_WITH_ARG(BM_math_fabs, "MATH_COMMON");
+
+static void BM_math_sincos(benchmark::State& state) {
+  d = 1.0;
+  while (state.KeepRunning()) {
+    double s, c;
+    sincos(d, &s, &c);
+    d += s + c;
+  }
+}
+BIONIC_BENCHMARK(BM_math_sincos);

Tomoms/android_bionic

benchmarks/math_benchmark.cpp
@@ -227,3 +227,13 @@ static void BM_math_fabs(benchmark::State& state) {
   SetLabel(state);
 }
 BENCHMARK_COMMON_VALS(BM_math_fabs);
+
+static void BM_math_sincos(benchmark::State& state) {
+  d = 1.0;
+  while (state.KeepRunning()) {
+    double s, c;
+    sincos(d, &s, &c);
+    d += s + c;
+  }
+}
+BENCHMARK(BM_math_sincos);